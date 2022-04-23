86ef20beedb8800f1297fd1d7511d566d4cf37a5
Baraa42 <75384314+Baraa42@users.noreply.github.com>
2022-04-23 13:19:11 +0200

    added deploy script

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -9,22 +9,22 @@ require("@nomiclabs/hardhat-etherscan");
 const { PRIVATE_KEY, ETHERSCAN_TOKEN, WEB3_INFURA_PROJECT_ID } = process.env;
 
 module.exports = {
-    solidity: "0.8.0",
+    solidity: "0.8.4",
     defaultNetwork: "hardhat",
     networks: {
         hardhat: {},
-        rinkeby: {
-            url: `https://rinkeby.infura.io/v3/${WEB3_INFURA_PROJECT_ID}`,
-            accounts: [`0x${PRIVATE_KEY}`],
-        },
+        // rinkeby: {
+        //     url: `https://rinkeby.infura.io/v3/${WEB3_INFURA_PROJECT_ID}`,
+        //     accounts: [`0x${PRIVATE_KEY}`],
+        // },
     },
     solidity: {
-        version: "0.8.0",
+        version: "0.8.4",
         settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200
-          }
+            optimizer: {
+                enabled: true,
+                runs: 200
+            }
         }
     },
     etherscan: {
