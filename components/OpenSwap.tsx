cc4a193ef7d4f7c82b9cadfac080bad19f32674d
00MB <s2061204@ed.ac.uk>
2022-04-23 14:30:29 +0200

    feat: index

---
+++ components/OpenSwap.tsx
@@ -0,0 +1,14 @@
+const OpenSwap = () => {
+return (
+<div className="rounded flex w-fit flex-row items-center bg-swapify-gray">
+    <div className="bg-swapify-purple">
+        <h1>+</h1>
+    </div>
+    <h1>
+        Create a new swap
+    </h1>
+</div>
+);
+};
+
+export default OpenSwap;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "swapify-purple": "#8247e5",
+        "swapify-gray": "#101418"
+      }
+    },
+  },
+  plugins: [],
+}
