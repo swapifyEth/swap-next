c5b98678fe439e6819a8f21c250251169c98e474
00MB <s2061204@ed.ac.uk>
2022-04-24 00:11:01 +0200

    styles v2

--- pages/index.tsx
+++ pages/index.tsx
@@ -181,10 +181,10 @@ export default function Home() {
 
     return (
         <>
-            <div className="min-h-screen w-4/5 mx-auto">
+            <div className="min-h-screen">
                 <Header active={HeaderActive.Swaps} />
                 {address ? (
-                    <div className="mx-4 my-8 w-full" data-aos="fade-up">
+                    <div className="mx-4 my-8 w-4/5 mx-auto" data-aos="fade-up">
                         <OpenSwap toggle={toggle} />
                         <div className="flex flex-col gap-y-6 pb-6 pt-4">
                             <h1 className="text-3xl">Your open swap</h1>
