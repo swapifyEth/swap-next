f0a7cf262875c890dba84fa6677913571fa79585
mattheweller <mattcjeller@gmail.com>
2022-04-24 08:04:16 +0200

    Deploy fix

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -11,7 +11,7 @@ return (
 <div>
     <h1 className="text-3xl mt-20 mb-4">Profile</h1>
     <div className="flex flex-row gap-x-3 my-6">
-        <Image alt=""  width="32" height="32" src="/Avatar.png" />
+        <img alt=""  width="32" height="32" src="/Avatar.png" />
         <p>0xjsx...fa12</p>
     </div>
     <div className="flex flex-col gap-y-6" data-aos="fade-in">
